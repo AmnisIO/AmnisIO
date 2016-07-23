3b8da0b140441bac630175f2c40fe0c4e4c21763
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-23 16:27:30 +0530

    Fix alignment of pin names

--- c/boards/riot_arduino_uno.c
+++ c/boards/riot_arduino_uno.c
@@ -27,14 +27,14 @@ riot_arduino_uno_sources* riot_arduino_uno_sources_create() {
         (D 0) PD0  2|    |27  PC4 (AI 4)
         (D 1) PD1  3|    |26  PC3 (AI 3)
         (D 2) PD2  4|    |25  PC2 (AI 2)
-    PWM+ (D 3) PD3 5|    |24  PC1 (AI 1)
+   PWM+ (D 3) PD3  5|    |24  PC1 (AI 1)
         (D 4) PD4  6|    |23  PC0 (AI 0)
               VCC  7|    |22  GND
               GND  8|    |21  AREF
               PB6  9|    |20  AVCC
               PB7 10|    |19  PB5 (D 13)
-  PWM+ (D 5) PD5  11|    |18  PB4 (D 12)
-  PWM+ (D 6) PD6  12|    |17  PB3 (D 11) PWM
+   PWM+ (D 5) PD5 11|    |18  PB4 (D 12)
+   PWM+ (D 6) PD6 12|    |17  PB3 (D 11) PWM
         (D 7) PD7 13|    |16  PB2 (D 10) PWM
         (D 8) PB0 14|    |15  PB1 (D 9) PWM
                     +----+
