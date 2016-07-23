21e818f9426d80edb2cb97188ef33047b0f1f5bf
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-23 14:27:05 +0530

    Add function declarations, usages and TODOs

--- c/boards/arduino_uno/riot_arduino_uno.c
+++ c/boards/arduino_uno/riot_arduino_uno.c
@@ -1,5 +1,6 @@
 #include "riot_arduino_uno.h"
 #include "utils.h"
+#include<stdbool.h>
 
 riot_arduino_uno_sinks* riot_arduino_uno_sinks_create() {
   riot_arduino_uno_sinks *sinks = xmalloc(sizeof(riot_arduino_uno_sinks));
@@ -10,6 +11,18 @@ riot_arduino_uno_sources* riot_arduino_uno_sources_create() {
   riot_arduino_uno_sources *sources = xmalloc(sizeof(riot_arduino_uno_sources));
   return sources;
 };
+
+void riot_arduino_uno_map_sinks_to_write_outputs(riot_arduino_uno_sinks *sinks) {
+  // TODO: Map sinks to write outputs 
+}
+
+void riot_arduino_uno_read_inputs(riot_arduino_uno_sources *sources) {
+  // TODO: Read all inputs and map to sources
+}
+
+bool riot_arduino_uno_is_sinks_empty(riot_arduino_uno_sinks *sinks) {
+  // TODO: Return true if no streams are present in sink, ie no output is required.
+}
 
 int riot_arduino_uno_run(riot_arduino_uno_main main) {
   if(main == NULL) {
@@ -23,9 +36,11 @@ int riot_arduino_uno_run(riot_arduino_uno_main main) {
   if(sinks == NULL) {
     return -3;
   }
-  while(true) {
-    // TODO: Read all inputs and map to sources
-    // TODO: If all sinks/outputs have completed, break
+  riot_arduino_uno_map_sinks_to_write_outputs(sinks);
+  while (true) {
+    riot_arduino_uno_read_inputs(sources);
+    if(riot_arduino_uno_is_sinks_empty(sinks))
+      break;
   }
   return 0;
 }
