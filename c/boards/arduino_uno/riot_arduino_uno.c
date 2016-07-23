b70c1ec7f3a81a044b1e42c0f25b8d083810a177
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-07-23 14:12:26 +0530

    Add basic riot_run function definition

--- c/boards/arduino_uno/riot_arduino_uno.c
+++ c/boards/arduino_uno/riot_arduino_uno.c
@@ -10,3 +10,22 @@ riot_arduino_uno_sources* riot_arduino_uno_sources_create() {
   riot_arduino_uno_sources *sources = xmalloc(sizeof(riot_arduino_uno_sources));
   return sources;
 };
+
+int riot_arduino_uno_run(riot_arduino_uno_main main) {
+  if(main == NULL) {
+    return -1;
+  }
+  riot_arduino_uno_sources *sources = riot_arduino_uno_sources_create();
+  if(sources == NULL) {
+    return -2;
+  }
+  riot_arduino_uno_sinks *sinks = main(sources);
+  if(sinks == NULL) {
+    return -3;
+  }
+  while(true) {
+    // TODO: Read all inputs and map to sources
+    // TODO: If all sinks/outputs have completed, break
+  }
+  return 0;
+}
