b9ca8742c637ea19233e557c0ea1e6c363fc87f1
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-11 12:53:52 +0530

    Add a blink sample for arduino uno to test

---
+++ ts/tests/boards/arduino-uno/blink.ts
@@ -0,0 +1,13 @@
+import { Sources, Sinks, HIGH, LOW, run, byte, createSinks } from '../../../src/boards/arduino-uno';
+
+function toggle(value: byte): byte {
+  return value == HIGH ? LOW : HIGH;
+}
+
+function blink(arduino: Sources): Sinks {
+  const sinks = createSinks();
+  sinks.LED$ = arduino.LED$.map(toggle);
+  return sinks;
+}
+
+run(blink);
