d7fdf33426596e5cc97fb568a4f6ca5ebd80cf18
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-14 00:58:35 +0530

    Fix issue with context not being updated

--- transpiler/src/emitter/source.ts
+++ transpiler/src/emitter/source.ts
@@ -2,10 +2,10 @@ import { EmitResult, emit } from './';
 import { Context } from '../contexts';
 import { SourceFile } from 'typescript';
 
-export const emitSourceFile = (node: SourceFile, context): EmitResult =>
+export const emitSourceFile = (node: SourceFile, context: Context): EmitResult =>
   node.statements
-    .reduce((result, node) => {
-      const emit_result = emit(node, context);
-      emit_result.emitted_string = result.emitted_string + '\n' + emit_result.emitted_string;
-      return emit_result;
+    .reduce<EmitResult>(({ context, emitted_string }, node) => {
+      const result = emit(node, context);
+      result.emitted_string = emitted_string + '\n' + result.emitted_string;
+      return result;
     }, { context, emitted_string: '' });
