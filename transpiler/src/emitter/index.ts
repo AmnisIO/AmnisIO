d494542241b32d2d9a75a5825aaee4cca0849990
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-14 01:32:32 +0530

    Add ability to blissfully ignore certain nodes

--- transpiler/src/emitter/index.ts
+++ transpiler/src/emitter/index.ts
@@ -3,13 +3,20 @@ import { emitImportDeclaration } from './imports';
 import { emitFunctionLikeDeclaration } from './functions';
 import { emitExpressionStatement, emitCallExpressionStatement } from './expressions';
 import { emitIdentifier, emitType } from './identifiers';
+import { emitBlock } from './blocks';
 import { emitSourceFile } from './source';
+import { emitReturnStatement } from './statements';
 import { Context } from '../contexts';
 
 export interface EmitResult {
   context: Context;
   emitted_string: string;
 }
+
+const ignore = (node: Node, context: Context): EmitResult => ({
+  context,
+  emitted_string: `ignored ${SyntaxKind[node.kind]}`
+})
 
 export const emit = (node: Node, context: Context): EmitResult => {
   switch (node.kind) {
@@ -20,7 +27,12 @@ export const emit = (node: Node, context: Context): EmitResult => {
     case SyntaxKind.CallExpression: return emitCallExpressionStatement(<any>node, context);
     case SyntaxKind.Identifier: return emitIdentifier(<any>node, context);
     case SyntaxKind.TypeReference: return emitType(<any>node, context);
+    case SyntaxKind.Block: return emitBlock(<any>node, context);
+    case SyntaxKind.ReturnStatement: return emitReturnStatement(<any>node, context);
     case SyntaxKind.EndOfFileToken: return { context, emitted_string: 'end' };
+    case SyntaxKind.ConditionalExpression: return ignore(node, context);
+    case SyntaxKind.VariableStatement: return ignore(node, context);
+    case SyntaxKind.BinaryExpression: return ignore(node, context);  
     default: throw new Error(`unknown syntax kind ${SyntaxKind[node.kind]}`);
   }
 };
