bacedb55d418ed05eb58f421ad621234a4aed570
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-14 12:37:56 +0530

    Sanitize variable names for C

--- transpiler/src/emitter/identifiers.ts
+++ transpiler/src/emitter/identifiers.ts
@@ -4,7 +4,9 @@ import { EmitResult, emit } from './';
 
 export const emitIdentifier = ({ text }: Identifier, context: Context): EmitResult => ({
   context,
-  emitted_string: context[text] == undefined ? text : context[text]
+  emitted_string: sanitize(context[text] == undefined ? text : context[text])
 });
 
 export const emitType = ({ typeName }: TypeReferenceNode, context: Context): EmitResult => emit(typeName, context);
+
+const sanitize = (value: string): string => value.replace(/\$/g, '');
