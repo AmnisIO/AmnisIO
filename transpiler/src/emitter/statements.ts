c47c1c3da508e5e5b5ea3a31417103a3befbbfd3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-14 12:02:30 +0530

    Add emit for variable statement

--- transpiler/src/emitter/statements.ts
+++ transpiler/src/emitter/statements.ts
@@ -1,4 +1,4 @@
-import { ReturnStatement } from 'typescript';
+import { ReturnStatement, VariableStatement } from 'typescript';
 import { Context } from '../contexts';
 import { EmitResult, emit } from './';
 
@@ -7,5 +7,19 @@ export const emitReturnStatement = ({ expression }: ReturnStatement, context: Co
   return {
     ...emit_result,
     emitted_string: `return ${emit_result.emitted_string};`
+  };
+};
+
+export const emitVariableStatement = ({ declarationList: { declarations } }: VariableStatement, context: Context): EmitResult => {
+  const emit_result =
+    declarations
+      .reduce<EmitResult>(({ context, emitted_string }, node) => {
+        const result = emit(node, context);
+        result.emitted_string = emitted_string + ';\n  ' + result.emitted_string;
+        return result;
+      }, { context, emitted_string: '' });
+  return {
+    ...emit_result,
+    emitted_string: `${emit_result.emitted_string};`
   };
 };
