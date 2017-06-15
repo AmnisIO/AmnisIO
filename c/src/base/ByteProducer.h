416b698b3ad87c8d562bcac4c0bbc0a6652b4b9d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-16 01:40:16 +0530

    Change formatting conventions

--- c/src/base/ByteProducer.h
+++ c/src/base/ByteProducer.h
@@ -7,11 +7,11 @@
 typedef uint8_t Byte;
 
 typedef struct ByteProducer {
-	void (*next)(struct ByteProducer *self, Byte v);
-	void (*error)(struct ByteProducer *self, Byte e);
-	void (*complete)(struct ByteProducer *self);
-	VariableLengthArray *listeners;
-  void (*init)();
+  void (*next) (struct ByteProducer *self, Byte v);
+  void (*error) (struct ByteProducer *self, Byte e);
+  void (*complete) (struct ByteProducer *self);
+  VariableLengthArray *listeners;
+  void (*init) ();
 } ByteProducer;
 
 #endif
