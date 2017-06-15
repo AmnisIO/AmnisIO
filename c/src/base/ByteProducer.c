538232d299748d40f8ba7b7faff9b145f733ac38
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-16 02:44:36 +0530

    Implement start/stop logic for ByteProducer

--- c/src/base/ByteProducer.c
+++ c/src/base/ByteProducer.c
@@ -7,7 +7,9 @@ static typedef struct ByteProducerImplementation {
   void (*add_listener) (ByteProducer *self, ByteListener *listener);
   void (*remove_listener) (ByteProducer *self, ByteListener *listener);
   VariableLengthArray *listeners;
-  void (*init) (struct ByteProducerImplementation *self);
+  void (*start) (struct ByteProducerImplementation *self);
+  void (*stop) (struct ByteProducerImplementation *self);
+  Byte started;
 } ByteProducerImplementation;
 
 static void _next (ByteProducer *self, Byte v) {
@@ -47,13 +49,27 @@ static void _complete (ByteProducer *self) {
 static void _add_listener (ByteProducer *self, ByteListener *listener) {
   ByteProducerImplementation *producer = (ByteProducerImplementation *) self;
   VariableLengthArray *listeners = producer->listeners;
-  listeners->push(listeners, listener);
+  listeners->push (listeners, listener);
+  if (listeners->length (listeners) > 0 && producer->started == 0) {
+    producer->started = 1;
+    if (producer->start != NULL) producer->start (producer);
+  }
 }
 
 static void _remove_listener (ByteProducer *self, ByteListener *listener) {
   ByteProducerImplementation *producer = (ByteProducerImplementation *) self;
   VariableLengthArray *listeners = producer->listeners;
-
+  int length = listeners->length (listeners);
+  for (int i = 0; i < length; i++) {
+    if (listeners->get (listeners, i) == listeners) {
+      listeners->remove (listeners, i);
+      break;
+    }
+  }
+  if (listeners->length (listeners) == 0 && producer->started == 1) {
+    producer->started == 0;
+    if (producer->stop != NULL) producer->stop (producer);
+  }
 }
 
 ByteProducer *byte_producer_create () {
@@ -63,6 +79,7 @@ ByteProducer *byte_producer_create () {
   producer->complete = _complete;
   producer->add_listener = _add_listener;
   producer->remove_listener = _remove_listener;
+  producer->started = 0;
   variable_length_array_initialize (&(producer->listeners));
   return (ByteProducer *) producer;
 }
