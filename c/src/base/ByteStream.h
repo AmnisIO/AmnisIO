cd7657dfe8959deec1988ad3a1f67a05857575dc
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-16 01:40:33 +0530

    Use new formatting conventions

--- c/src/base/ByteStream.h
+++ c/src/base/ByteStream.h
@@ -6,10 +6,10 @@
 #include "ByteProducer.h"
 
 typedef struct ByteStream {
-	void (*next)(struct ByteStream *self, Byte v);
-	void (*error)(struct ByteStream *self, Byte e);
-	void (*complete)(struct ByteStream *self);
-	VariableLengthArray *listeners;
+  void (*next) (struct ByteStream *self, Byte v);
+  void (*error) (struct ByteStream *self, Byte e);
+  void (*complete) (struct ByteStream *self);
+  VariableLengthArray *listeners;
 } ByteStream;
 
 ByteStream* byte_stream_create(ByteProducer *producer);
