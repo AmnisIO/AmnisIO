eec242e119f8ddaa434e5b2d7db046105781a7c5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-16 02:48:23 +0530

    Add listener to producer on creation

--- c/src/base/ByteStream.c
+++ c/src/base/ByteStream.c
@@ -4,35 +4,39 @@ static void _next (ByteStream *stream, Byte v) {
   VariableLengthArray *array = stream->listeners;
   int length = array->length (array);
   for (int i = 0; i < length; i++) {
-    ByteStream *listener = ((ByteStream *) array->get (array, i));
+    ByteListener *listener = ((ByteListener *) array->get (array, i));
     listener->next (listener, v);
   }
 };
 
 static void _error (ByteStream *stream, Byte e) {
-  VariableLengthArray *array = stream->listeners;
-  int length = array->length (array);
+  VariableLengthArray *listeners = stream->listeners;
+  int length = listeners->length (listeners);
   for (int i = 0; i < length; i++) {
-    ByteStream *listener = ((ByteStream *) array->get (array, i));
+    ByteListener *listener = ((ByteListener *) listeners->get (listeners, i));
     listener->error (listener, e);
   }
+  listeners->free (listeners);
+  free (listeners);
 };
 
 static void _complete (ByteStream *stream) {
-  VariableLengthArray *array = stream->listeners;
-  int length = array->length (array);
+  VariableLengthArray *listeners = stream->listeners;
+  int length = listeners->length (listeners);
   for (int i = 0; i < length; i++) {
-    ByteStream *listener = ((ByteStream *) array->get (array, i));
+    ByteListener *listener = ((ByteListener *) listeners->get (listeners, i));
     listener->complete (listener);
   }
+  listeners->free (listeners);
+  free (listeners);
 };
 
-ByteStream* byte_stream_create (ByteProducer *producer) {
+ByteStream *byte_stream_create (ByteProducer *producer) {
   ByteStream *stream = xmalloc (sizeof (ByteStream));
   stream->next = _next;
   stream->error = _error;
   stream->complete = _complete;
   variable_length_array_initialize (&(stream->listeners));
-  producer->listeners->push (producer->listeners, stream);
+  producer->add_listener (producer, (ByteListener *) stream);
   return stream;
 }
