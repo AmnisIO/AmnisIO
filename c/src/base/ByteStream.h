318954f2dec6116b3858e36231502d6cb5b4a6f4
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-17 17:56:03 +0530

    Make Stream<Byte> implement InternalListener<Byte>

--- c/src/base/ByteStream.h
+++ c/src/base/ByteStream.h
@@ -14,19 +14,20 @@ typedef Byte (*byte_steam_map_function) (Byte value);
 typedef Boolean (*byte_steam_filter_function) (Byte value);
 
 typedef struct ByteStream {
-  void (*add_listener) (struct ByteStream *self, ByteListener *listener);
-  void (*remove_listener) (struct ByteStream *self, ByteListener *listener);
-  void *(*subscribe) (struct ByteStream *self, ByteListener *listener);
-  ByteSubscription *(*unsubscribe) (struct ByteStream *self, ByteListener *listener);
-  ByteListenerInternal *_internal_listener;
+  void (*_next) (struct ByteListenerInternal *self, Byte v);
+  void (*_error) (struct ByteListenerInternal *self, int e);
+  void (*_complete) (struct ByteListenerInternal *self);
   ByteProducerInternal *_producer;
   VariableLengthArray *_internal_listeners;
   int _stop_id;
-  int _error;
+  int _error_code;
   void (*_teardown) (struct ByteStream *self);
   void (*_stop_now) (struct ByteStream *self);
   void (*_add) (struct ByteStream *self, ByteListenerInternal *listener);
   void (*_remove) (struct ByteStream *self, ByteListenerInternal *listener);
+  void (*add_listener) (struct ByteStream *self, ByteListener *listener);
+  void (*remove_listener) (struct ByteStream *self, ByteListener *listener);
+  ByteSubscription *(*subscribe) (struct ByteStream *self, ByteListener *listener);
   struct ByteStream *(*map) (struct ByteStream *self, byte_steam_map_function map);
   struct ByteStream *(*mapTo) (struct ByteStream *self, Byte value);
   struct ByteStream *(*filter) (struct ByteStream *self, byte_steam_filter_function filter);
@@ -40,7 +41,5 @@ ByteStream *byte_stream_never ();
 ByteStream *byte_stream_empty ();
 ByteStream *byte_stream_throw ();
 ByteStream *byte_stream_from_array (Byte array[]);
-ByteStream *byte_stream_periodic (int milliseconds);
-ByteStream *byte_stream_merge (ByteStream streams[]);
 
 #endif // C_BYTESTREAM_H
