8409b9387e30d9b483122f5855bcafb154035231
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-17 16:29:00 +0530

    Reintroduce Subscription<T>

--- c/src/base/ByteStream.h
+++ c/src/base/ByteStream.h
@@ -3,12 +3,13 @@
 
 #include "ByteListener.h"
 #include "ByteProducer.h"
+#include "ByteSubscription.h"
 
 typedef struct ByteStream {
   void (*add_listener) (struct ByteStream *self, ByteListener *listener);
   void (*remove_listener) (struct ByteStream *self, ByteListener *listener);
   void *(*subscribe) (struct ByteStream *self, ByteListener *listener);
-  void *(*unsubscribe) (struct ByteStream *self, ByteListener *listener);
+  ByteSubscription *(*unsubscribe) (struct ByteStream *self, ByteListener *listener);
 } ByteStream;
 
 ByteStream *byte_stream_create (ByteProducer *producer);
