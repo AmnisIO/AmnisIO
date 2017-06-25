867bf0ceb31e5847192dcee6096116f8b5947fd1
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-06-26 03:49:12 +0530

    Restructure AmnisIO internals

---
+++ configs/arduino-uno.ts
@@ -0,0 +1,7 @@
+import { BoardConfiguration } from './types';
+
+export const config: BoardConfiguration = {
+  platformio_identifier: 'uno',
+  gyrus_repository: 'https://github.com/AmnisIO/gyrus',
+  gyrus_directory: 'arduino_uno'
+};
---
+++ configs/types.ts
@@ -0,0 +1,5 @@
+export interface BoardConfiguration {
+  platformio_identifier: string;
+  gyrus_repository: string;
+  gyrus_directory: string;
+}
